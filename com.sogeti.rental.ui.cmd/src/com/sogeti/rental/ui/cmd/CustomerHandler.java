c0515661dd125abb5b4f5850debb4fa865e2dece
esamuel <esamuel@FR-31-01-14-053.corp.capgemini.com>
2017-03-09 17:01:38 +0100

    Add copy paste

---
+++ com.sogeti.rental.ui.cmd/src/com/sogeti/rental/ui/cmd/CustomerHandler.java
@@ -0,0 +1,54 @@
+package com.sogeti.rental.ui.cmd;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.dnd.Clipboard;
+import org.eclipse.swt.dnd.RTFTransfer;
+import org.eclipse.swt.dnd.TextTransfer;
+import org.eclipse.swt.dnd.Transfer;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.opcoach.training.rental.Customer;
+
+public class CustomerHandler extends AbstractHandler implements IHandler {
+
+	public CustomerHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+        System.out.println("copy customer");
+        String textData = "";
+		ISelection currentSelection = HandlerUtil.getCurrentSelection(event);
+		
+		if (currentSelection instanceof IStructuredSelection)
+		{
+			Object selected = ((IStructuredSelection) currentSelection).getFirstElement();
+
+			if (selected instanceof Customer)
+			{
+				textData = ((Customer)selected).getDisplayName();
+			}
+			
+		}
+		
+	 	Clipboard clipboard = new Clipboard(Display.getCurrent());
+		String rtfData = "{\\rtf1\\b\\i Hello World}";
+		TextTransfer textTransfer = TextTransfer.getInstance();
+		RTFTransfer rtfTransfer = RTFTransfer.getInstance();
+		Transfer[] transfers = new Transfer[]{textTransfer, rtfTransfer};
+		Object[] data = new Object[]{textData, rtfData};
+		clipboard.setContents(data, transfers);
+		clipboard.dispose();
+	 
+		
+		return null;
+	}
+
+}
