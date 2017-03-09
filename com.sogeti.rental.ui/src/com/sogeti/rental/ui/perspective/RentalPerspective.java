d41ff7608ff302b69a595e9de4e755c9022451fe
esamuel <esamuel@FR-31-01-14-053.corp.capgemini.com>
2017-03-09 09:47:23 +0100

    Ajout Perspective

---
+++ com.sogeti.rental.ui/src/com/sogeti/rental/ui/perspective/RentalPerspective.java
@@ -0,0 +1,39 @@
+package com.sogeti.rental.ui.perspective;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class RentalPerspective implements IPerspectiveFactory {
+
+	/**
+	 * Creates the initial layout for a page.
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		String editorArea = layout.getEditorArea();
+		addFastViews(layout);
+		addViewShortcuts(layout);
+		addPerspectiveShortcuts(layout);
+		layout.addView("com.sogeti.rental.ui.viewRental", IPageLayout.LEFT, 0.91f, IPageLayout.ID_EDITOR_AREA);
+		layout.addView("com.sogeti.rental.ui.AgencyView", IPageLayout.LEFT, 0.52f, "com.sogeti.rental.ui.viewRental");
+	}
+
+	/**
+	 * Add fast views to the perspective.
+	 */
+	private void addFastViews(IPageLayout layout) {
+	}
+
+	/**
+	 * Add view shortcuts to the perspective.
+	 */
+	private void addViewShortcuts(IPageLayout layout) {
+	}
+
+	/**
+	 * Add perspective shortcuts to the perspective.
+	 */
+	private void addPerspectiveShortcuts(IPageLayout layout) {
+	}
+
+}
